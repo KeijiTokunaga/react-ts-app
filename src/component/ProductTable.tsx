f81dded2ffa2ebaddead4ac27a2b6b602cf03b6b
Keiji Tokunaga <keizi81@gmail.com>
2021-05-07 19:10:29 +0900

    step2を関数コンポーネントで実現

--- src/component/ProductTable.tsx
+++ src/component/ProductTable.tsx
@@ -14,6 +14,22 @@ type ProductProps = {
 };
 
 export const ProductTable: React.FC<ProductProps> = (props) => {
+  const rows: JSX.Element[] = [];
+  let lastCategory: string = '';
+
+  props.products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />,
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
   return (
     <table>
       <thead>
@@ -22,14 +38,7 @@ export const ProductTable: React.FC<ProductProps> = (props) => {
           <th>Price</th>
         </tr>
       </thead>
-      <tbody>
-        {props.products.map((v) => (
-          <>
-            <ProductCategoryRow category={v.category} key={v.category} />
-            <ProductRow product={v} key={v.name} />
-          </>
-        ))}
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 };
